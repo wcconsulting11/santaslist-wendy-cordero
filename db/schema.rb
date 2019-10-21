ad48088dcb0bff651ad0ccba3edb0bc59dde1805
Wendy Cordero <wcconsulting11@gmail.com>
2019-10-21 17:13:56 +0000

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2019_10_15_013949) do
   create_table "lists", force: :cascade do |t|
     t.string "name"
     t.integer "age"
-    t.text "request"
+    t.text "wish"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
